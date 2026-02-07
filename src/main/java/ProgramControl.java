4e5144aa440f2ee2135f45c4645de585f7c807d8
Patrick <164663789+Psujj@users.noreply.github.com>
2026-02-07 15:01:32 -0500

    added test cases and main

---
+++ src/main/java/ProgramControl.java
@@ -0,0 +1,8 @@
+public class ProgramControl {
+    public void displayFilesListed() {
+
+    }
+    public void displayFileContents(int index) {
+
+    }
+}
--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -1,5 +1,28 @@
 /**
  * Commmand Line Utility
  */
+
 public class TopSecret {
+    static ProgramControl control = new ProgramControl();
+    public static void main(String[] args) {
+        if (args.length == 0){
+            control.displayFilesListed();
+            return;
+        }
+        if(args.length >= 2){
+            System.err.println("Error too many arguments");
+            return;
+        }
+
+        int fileIndex;
+        try{
+            fileIndex = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e){
+            System.err.println("Error in file index");
+            return;
+        }
+
+        control.displayFileContents(fileIndex); // if arg length == 1
+    }
+
 }
